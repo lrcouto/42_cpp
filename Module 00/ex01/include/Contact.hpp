a3c55e751d36123c2a07d72c94066c6c6152f9fc
laura.couto <laura.couto@piposaude.com.br>
2021-11-03 18:43:15 -0300

    Classes

--- Module 00/ex01/include/Contact.hpp
+++ Module 00/ex01/include/Contact.hpp
@@ -6,18 +6,39 @@
 /*   By: lcouto <lcouto@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/25 22:45:35 by lcouto            #+#    #+#             */
-/*   Updated: 2021/10/25 22:49:58 by lcouto           ###   ########.fr       */
+/*   Updated: 2021/11/03 18:42:44 by lcouto           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # ifndef CONTACT_HPP
 # define CONTACT_HPP
 
-class Contact
+# include <iostream>
+
+class	Contact
 {
 	private:
+		std::string	_firstName;
+		std::string	_lastName;
+		std::string	_phoneNumber;
+		std::string	_nickname;
+		std::string	_darkestSecret;
 
 	public:
+		void setFirstName(std::string buffer);
+		void setLastName(std::string buffer);
+		void setNickName(std::string buffer);
+		void setPhoneNumber(std::string buffer);
+		void setDarkestSecret(std::string buffer);
+
+		std::string getFirstName(void);
+		std::string getLastName(void);
+		std::string getNickName(void);
+		std::string getPhoneNumber(void);
+		std::string getDarkestSecret(void);
+
+		Contact(void);
+		~Contact(void);
 };
 
 #endif
--- Module 00/ex01/include/PhoneBook.hpp
+++ Module 00/ex01/include/PhoneBook.hpp
@@ -6,21 +6,24 @@
 /*   By: lcouto <lcouto@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/25 22:43:53 by lcouto            #+#    #+#             */
-/*   Updated: 2021/10/26 16:24:27 by lcouto           ###   ########.fr       */
+/*   Updated: 2021/11/03 18:42:46 by lcouto           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # ifndef PHONEBOOK_HPP
 # define PHONEBOOK_HPP
 
+# include <Contact.hpp>
+
 class	PhoneBook
 {
 	private:
+	Contact	_contacts[8];
+	int		_insertionIndex;
 
 	public:
 		PhoneBook(void);
 		~PhoneBook(void);
-
 };
 
 #endif
