bb3fdcc88dd04266f5b3220e333b9b7778ae6e25
laura.couto <laura.couto@piposaude.com.br>
2021-11-12 18:02:53 -0300

    account

---
+++ Module 00/ex02/src/Account.cpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lcouto <lcouto@student.42sp.org.br>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/12 14:17:22 by lcouto            #+#    #+#             */
+/*   Updated: 2021/11/12 18:02:53 by lcouto           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+
+int	_nbAccounts = 0;
+int	_totalAmount = 0;
+int	_totalNbDeposits = 0;
+int	_totalNbWithdrawals = 0;
+
+int	Account::getNbAccounts(void)
+{
+	return (Account::_nbAccounts);
+}
+
+int	Account::getTotalAmount(void)
+{
+	return (Account::_totalAmount);
+}
+
+int	Account::getNbDeposits(void)
+{
+	return (Account::_totalNbDeposits);
+}
+
+int	Account::getNbWithdrawals(void)
+{
+	return (Account::_totalNbWithdrawals);
+}
