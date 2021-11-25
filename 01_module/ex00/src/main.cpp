de30a09a223b6e29a57f71d71cea525b0d25ed88
lrcouto <laurarccouto@gmail.com>
2021-11-25 10:11:43 -0300

    tired tired tired

--- 01_module/ex00/src/main.cpp
+++ 01_module/ex00/src/main.cpp
@@ -1,34 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lcouto <lcouto@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/22 14:55:59 by lcouto            #+#    #+#             */
-/*   Updated: 2021/11/24 21:37:36 by lcouto           ###   ########.fr       */
+/*   Created: 2021/11/24 22:29:04 by lcouto            #+#    #+#             */
+/*   Updated: 2021/11/24 22:29:05 by lcouto           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# ifndef ZOMBIE_HPP
-# define ZOMBIE_HPP
-
-# include <iostream>
-
-class Zombie
-{
-	private:
-		std::string _name;
-	
-	public:
-		Zombie(std::string name);
-		Zombie(void);
-		~Zombie(void);
-
-		void	announce(void);
-};
-
-Zombie	*newZombie(std::string name);
-void	randomChump(std::string name);
-
-#endif
