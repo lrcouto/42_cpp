06e6a9c450deadd43cabd0ab2ba81788af53487c
lrcouto <laurarccouto@gmail.com>
2022-06-15 02:44:55 -0300

    Fix logic error

--- 01_module/ex06/src/main.cpp
+++ 01_module/ex06/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lcouto <lcouto@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 22:29:04 by lcouto            #+#    #+#             */
-/*   Updated: 2022/06/15 02:19:11 by lcouto           ###   ########.fr       */
+/*   Updated: 2022/06/15 02:43:07 by lcouto           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,39 +30,39 @@ int	main(int argc, char **argv)
 			index = i;
 	}
 
-	switch (index)
+	while (index <= 3)
 	{
-		case 0:
-			std::cout << std::endl;
-			harl.complain("DEBUG");
-			std::cout << std::endl;
-			break;
-		case 1:
-			std::cout << std::endl;
-			harl.complain("INFO");
-			std::cout << std::endl;
-			break;
-		case 2:
-			std::cout << std::endl;
-			harl.complain("WARNING");
-			std::cout << std::endl;
-			break;
-		case 3:
-			std::cout << std::endl;
-			harl.complain("ERROR");
-			std::cout << std::endl;
-			break;
-		default:
-			std::cout << std::endl;
-			std::cout << "\033[0;32mHarl absent-mindedly picks his nose and grumbles something about bacon. (￣～￣;)\033[0m" << std::endl;
-			std::cout << std::endl;
-			break;
+		switch (index)
+		{
+			case 0:
+				std::cout << std::endl;
+				harl.complain("DEBUG");
+				std::cout << std::endl;
+				break;
+			case 1:
+				std::cout << std::endl;
+				harl.complain("INFO");
+				std::cout << std::endl;
+				break;
+			case 2:
+				std::cout << std::endl;
+				harl.complain("WARNING");
+				std::cout << std::endl;
+				break;
+			case 3:
+				std::cout << std::endl;
+				harl.complain("ERROR");
+				std::cout << std::endl;
+				break;
+			default:
+				std::cout << std::endl;
+				std::cout << "\033[0;32mHarl absent-mindedly picks his nose and grumbles something about bacon. (￣～￣;)\033[0m" << std::endl;
+				std::cout << std::endl;
+				index = 4;
+				break;
+		}
+	index++;
 	}
-
-	// This is kinda silly, forcing usage of a switch statement to effectively compare strings, 
-	// forcing me to write some convoluted logic to get the index of another string while not 
-	// allowing me to use any methods that would find the index without iterating through the string.
-	// IDK typical 42 stuff.
-
+	
 	return (0);
 }
