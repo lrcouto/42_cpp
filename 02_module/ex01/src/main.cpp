94e41f553851fe0c0f28df7b37aeaf8e55ee35d4
lrcouto <laurarccouto@gmail.com>
2022-06-17 22:00:47 -0300

    Add ex01 fixed point class

--- 02_module/ex01/src/main.cpp
+++ 02_module/ex01/src/main.cpp
@@ -6,24 +6,39 @@
 /*   By: lcouto <lcouto@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/16 02:06:58 by lcouto            #+#    #+#             */
-/*   Updated: 2022/06/16 02:35:26 by lcouto           ###   ########.fr       */
+/*   Updated: 2022/06/17 21:59:28 by lcouto           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Fixed.hpp"
+# include "Fixed.hpp"
 # include <iostream>
 
 int	main(void)
 {
 	Fixed a;
-	Fixed b(a);
-	Fixed c;
+	Fixed const b(10);
+	Fixed const c(42.42f);
+	Fixed const d(b);
 
-	c = b;
+	std::cout << "\n--------------------\n" << std::endl;
 
-	std::cout << a.getRawBits() << std::endl;
-	std::cout << b.getRawBits() << std::endl;
-	std::cout << c.getRawBits() << std::endl;
+	a = Fixed(1234.4321f);
+
+	std::cout << "\n--------------------\n" << std::endl;
+
+	std::cout << "a is " << a << std::endl;
+	std::cout << "b is " << b << std::endl;
+	std::cout << "c is " << c << std::endl;
+	std::cout << "d is " << d << std::endl;
+
+	std::cout << "\n--------------------\n" << std::endl;
+
+	std::cout << "a is " << a.toInt() << " as Integer" << std::endl;
+	std::cout << "b is " << b.toInt() << " as Integer" << std::endl;
+	std::cout << "c is " << c.toInt() << " as Integer" << std::endl;
+	std::cout << "d is " << d.toInt() << " as Integer" << std::endl;
+
+	std::cout << "\n--------------------\n" << std::endl;
 	
 	return (0);
 }
